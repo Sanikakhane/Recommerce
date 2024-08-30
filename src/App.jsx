84229199a8136d653adbc3f874f89714768e07d4
Sanika <sanikakhane@gmail.com>
2024-08-31 00:02:08 +0530

    Frontend1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 // import { Homepage } from './component/HomePage/Homepage'
-//import { Homepage } from './component/After_Login/Homepage'
-import LoginSignUp from './component/LoginSignUp/LoginSignUp'
+import { Homepage } from './component/After_Login/Homepage'
+// import LoginSignUp from './component/LoginSignUp/LoginSignUp'
+// import { NavbarAfterLogin } from './component/After_Login/NavbarAfterLogin'
+
+import { Navbar } from './component/HomePage/Navbar'
+
+
 function App() {
   return (
     <div>
-         <LoginSignUp/>
+          <Navbar/>
+         <Homepage/>
     </div>
   )
 }
